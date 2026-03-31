32280026fcfcbf49cb45a38d701019aaa57e2e32
bazooka19955 <hussein@inboxf.com>
2026-03-31 06:10:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const transporter = nodemailer.createTransport({
   port: 2525,
   secure: false,
   auth: {
-    user: 'hussein1995',
+    user: 'hussein1995@hussein.my',
     pass: 'Aa19955Aa@@'
   },
   tls: { rejectUnauthorized: false }
